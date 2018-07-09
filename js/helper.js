8c4d93da202629575766867b5a872a5fd7fd599d
J0ssue <jdiaz66@icloud.com>
2018-07-09 16:54:00 +0100

    finished header section

--- js/helper.js
+++ js/helper.js
@@ -12,23 +12,26 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
+// ***** Header ***** //
 var HTMLheaderName = '<h1 id="name">Josue Diaz</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span>Web Developer</span><hr>';
 
+// Header Contacts
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">+351-924-252-686</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">jdiaz66@icloud.com</span></li>';
+var HTMLlinkedIn= '<li class="flex-item"><span class="orange-text">linkedIn</span><a class="contactLink" href="https://www.linkedin.com/in/josue-diaz-67648a159/" target="_blank"><span class="white-text">Josue</span></a></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">https://github.com/J0ssue</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">Portugal</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLbioPic = '<img src="images/josue.png" class="biopic">';
+var HTMLwelcomeMsg = '<span class="welcome-message">Hello, my name is Josue, I\'m a Software Developer. Welcome to my resume</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
+// ** Work ** //
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,51 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+let octopus = {
+	init: function() {
+		bio.init();
+	}
+};
+
+const bio = {
+	init: function() {
+		this.display();
+	},
+	name : HTMLheaderName,
+	role : HTMLheaderRole,
+	contacts : {
+		mobile: HTMLmobile,
+		email: HTMLemail,
+		github: HTMLgithub,
+		linkedIn: HTMLlinkedIn, 
+		location: HTMLlocation,
+	},
+	welcomeMessage: HTMLwelcomeMsg, 
+	skillTitle: HTMLskillsStart,
+	skillLi: HTMLskills,
+	skills: ['JavaScript', 'HTML', 'CSS'],
+	biopic: HTMLbioPic,
+	display: function() {
+		// ** Header ** //
+		$('#header').prepend(this.role);
+		$('#header').prepend(this.name);
+
+		// Contacts/header
+		Object.values(this.contacts).forEach(function(contact) {
+			$('#topContacts').append(contact);
+		});
+
+		// Pic/header 
+		$('#header').append(this.biopic);
+
+		// welcome/header
+		$('#header').append(this.welcomeMessage);
+
+		// Skills/header
+		$('#header').append(this.skillTitle);
+		for (let i = 0; i < this.skills.length; i++) {
+		let replaceSkill = this.skillLi.replace('%data%', this.skills[i]);;
+			$('#skills').append(replaceSkill);
+		}
+	},
+}; 
+
+octopus.init();
+
