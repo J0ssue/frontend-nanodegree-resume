effcec3dbcda7fd558d9247a773b86897b584fc2
J0ssue <jdiaz66@icloud.com>
2018-07-09 18:42:26 +0100

    added first job entry

--- js/helper.js
+++ js/helper.js
@@ -39,6 +39,7 @@ var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
+// ** Project ** //
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,11 @@
 let octopus = {
 	init: function() {
 		bio.init();
+		work.display();
 	}
 };
 
+// ** Header ** //
 const bio = {
 	init: function() {
 		this.display();
@@ -46,6 +48,36 @@ const bio = {
 		}
 	},
 }; 
+
+// ** Work ** //
+
+let work = {
+	init: function() {
+		this.display();
+	},
+	jobs: {
+		workStart: HTMLworkStart,
+		employer: HTMLworkEmployer, 
+		title: HTMLworkTitle, 
+		location: HTMLworkLocation, 
+		dates: HTMLworkStart + HTMLworkDates, 
+		description: HTMLworkDescription,
+	},
+	display: function() {
+		// ** Work Experience:
+		const employer = this.jobs.employer.replace('%data%', 'Adecco');
+		const title = this.jobs.title.replace('%data%', 'Operador Producção M1')
+		const location = this.jobs.location.replace('%data%', 'Carregado, Portugal'); 
+		const dates = this.jobs.dates.replace('%data%', '5/2018 - in progress'); 
+		const description = this.jobs.description.replace('%data%', 'Executa tarefas de estampagem, montagem e desmontagem de ferraments, afinação de ferramentas, montagem de subconjuntos movimentação de contentores e embalagem.');
+
+		$('#workExperience').append(this.jobs.workStart);
+		$('.work-entry').append(employer + title);	
+		$('.work-entry').append(location);	
+		$('.work-entry').append(dates);	
+		$('.work-entry').append(description);	
+	}
+};
 
 octopus.init();
 
